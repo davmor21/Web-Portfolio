1dad4b821d0642259d1bbc3a0d0b5a9c07b11e4a
Davon Morris <davm2142@gmail.com>
2025-07-07 21:08:49 -0500

    changes

--- dist/assets/index-BiUOU9_J.js
+++ dist/assets/index-BiUOU9_J.js
@@ -1,8 +1,4 @@
-import './style.css'
-
-const app = document.querySelector('#app')
-
-app.innerHTML = `
+(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const c=document.querySelector("#app");c.innerHTML=`
   <div class="container">
     <!-- Intro Hero -->
     <div class="intro">
@@ -67,4 +63,4 @@ app.innerHTML = `
       </ul>
     </section>
   </div>
-`
+`;
