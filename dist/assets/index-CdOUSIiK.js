023bf960c7cca7079b63073ef5193fcfd5f0d397
Davon Morris <davm2142@gmail.com>
2025-07-07 20:38:08 -0500

    test

---
+++ dist/assets/index-CdOUSIiK.js
@@ -0,0 +1,15 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const n=document.querySelector("#app");n.innerHTML=`
+  <section class="intro">
+    <h1>Davon Morris</h1>
+    <p>
+      Software Developer
+    </p>
+  </section>
+
+  <!-- Later you can add more sections: -->
+  <!--
+    <section id="about">…</section>
+    <section id="projects">…</section>
+    <section id="contact">…</section>
+  -->
+`;
